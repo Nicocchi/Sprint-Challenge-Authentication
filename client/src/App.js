1cf396011272ee927393a096a1b842241d87d803
Nicocchi <nicocchilover@gmail.com>
2018-10-19 12:03:53 -0400

    added home component in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, withRouter } from 'react-router-dom';
+
 import './App.css';
+
+const Home = props => {
+  return (
+      <div>
+        <h1>Home Component</h1>
+      </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <main>
+            <Route exact path="/" component={Home} />
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
